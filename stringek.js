766fbd50eb6fb57e31d3329446045534be997014
Rádi Olivér <radi.oliver@diak.szbi-pg.hu>
2025-03-25 10:40:25 +0100

    sfg

---
+++ stringek.js
@@ -0,0 +1,43 @@
+console.log("Hello")
+
+let s = "karakterlánc \"Hello\" ";
+console.log(s);
+
+let ujsor = "karakterlánc \n \"Hello\" ";
+console.log(ujsor);
+
+let kocsi = "karakterlánc \r \"Hello\" ";
+console.log(kocsi);  
+
+console.log(ujsor)
+console.log(ujsor.length)
+
+//for ciklus index-el
+let s2 = "JavaScript";
+for(let i = 0; i < s2.length; i++) {
+console.log(s2[i]);
+}
+
+//for of ciklus index nélkül
+let s3 = "MySql";
+for(betu of s3){
+    console.log(betu);
+}
+
+//szövegösszefűzés
+console.log(s2 + s3);
+console.log(s2 + " " + s3);
+console.log(`${s2} ${s3}`);
+
+//concat method haszálata:
+let szövegösszefűzés = s2.concat(" ", s3);
+console.log(szövegösszefűzés)
+
+// console.log(s2.toUpperCase());
+// console.log(s2.toLowerCase());
+
+console.log(s3.charAt(4));
+console.log(s3.charCodeAt(4));
+
+console.log(s3.startsWith("m"));
+console.log(s3.endsWith("l"));
